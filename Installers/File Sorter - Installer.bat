ee0c51e3b57281d963b86fa121bdd40d383bbe8a
RomsiVmax <romeo.seeger@online.de>
2026-07-30 17:17:56 +0200

    Update File Sorter - Installer.bat

--- Installers/File Sorter - Installer.bat
+++ Installers/File Sorter - Installer.bat
@@ -7,8 +7,8 @@ fltmc >nul 2>&1 || (
 
     echo Press any key to install the program! NOTE: You NEED an active internet connection and you NEED to click "Yes" on all warnings
     pause >nul
-    cd C:/Program Files/PowerShell/7
-    pwsh.exe -Command "Start-Process cmd -ArgumentList '/c ""%~f0""' -Verb RunAs
+    cd C:\Windows\System32\WindowsPowerShell\v1.0\
+    powershell.exe -Command "Start-Process cmd -ArgumentList '/c ""%~f0""' -Verb RunAs
     goto install
 )
 goto install
